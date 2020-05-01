9dac2bdf8ccc4abac07e2bbf9a3502a5d3b7aec9
Ihfaz <ihfaz.98@gmail.com>
2020-05-01 03:00:22 -0500

    Color palette updated

--- app/src/main/java/zunayed/siddiqui/utdallas/edu/mapapp/ListActivity.java
+++ app/src/main/java/zunayed/siddiqui/utdallas/edu/mapapp/ListActivity.java
@@ -43,7 +43,7 @@ public class ListActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
 
-        fetchData("Bangladesh", "country", String.valueOf(LocalDate.now()));    // District cases
+        fetchData("Bangladesh", "country", String.valueOf(LocalDate.of(2020, 04, 29)));    // District cases
 //        fetchData("Dhaka", "city", String.valueOf(LocalDate.now()));            // Dhaka zone cases
     }
 
@@ -53,9 +53,12 @@ public class ListActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
     }
 
+    // This method is used to get the data about covid cases from the API. It uses the AsyncTask class.
     protected void fetchData(String name, String type, String date) {
+        // Parameters for the API
         String params = "name="+name+"&type="+type+"&date="+date;
 
+        // Creating a DataFetcher object to be executed as a separate thread
         DataFetcher df = new DataFetcher() {
             //Function that is called from the AsyncTask before the work is started.
             @Override
@@ -67,32 +70,40 @@ public class ListActivity extends AppCompatActivity {
             @Override
             public void onPostExecute(JSONObject result){
                 if(result != null) {
-                    populateList(result);
+                    populateList(result);   // Populate the list if a result is fetched
                 }
                 else{
                     //We didn't get a response we expected.
-                    Toast.makeText(getApplicationContext(), "Error!!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Error! Couldn't fetch data.", Toast.LENGTH_SHORT).show();
                 }
 
                 //TODO: Hide the spinner
             }
         };
 
+        // Execute the thread
         df.execute(params);
     }
 
+    // This method is used to parse the JSON from the API into a list of cases
     protected ArrayList<Case> parseJson(JSONObject jsonObject) {
         ArrayList<Case> caseList = new ArrayList<>();
 
         try {
-            JSONObject payload = jsonObject.getJSONObject("payload");      // JSON object which contains all the data
+            if (jsonObject.getString("status").equals("error")) {
+                Toast.makeText(getApplicationContext(), "Error! Couldn't find data.", Toast.LENGTH_SHORT).show();
+                return null;
+            }
+
+            JSONObject payload = jsonObject.getJSONObject("payload");     // JSON object which contains all the data
             String level = payload.getString("level");              // Determines if city or zone
             JSONArray data = payload.getJSONArray("data");          // JSON array which contains the list of city/zone-cases
 
+            // Loop through the JSON objects inside the array
             for (int i = 0; i < data.length(); i++) {
-                JSONObject obj = data.getJSONObject(i);
-                String loc = (level.equals("city") ? "city" : "zone");
-                caseList.add(new Case(obj.getString(loc), obj.getInt("cases")));
+                JSONObject obj = data.getJSONObject(i); // Get objects at each array index
+                String loc = (level.equals("city") ? "city" : "zone");  // Determines whether it is a city or a Dhaka zone
+                caseList.add(new Case(obj.getString(loc), obj.getInt("cases")));    // Adds each case to a list
             }
         } catch (JSONException e) {
             e.printStackTrace();
@@ -107,6 +118,6 @@ public class ListActivity extends AppCompatActivity {
             }
         });
 
-        return caseList;
+        return caseList;    // Return the list created
     }
 }
